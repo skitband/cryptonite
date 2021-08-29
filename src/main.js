16ee59d0f08b96fc8248e65d53290c54bfe35bd1
slio <skitband00@gmail.com>
2021-08-29 21:29:09 +0800

    make it better

--- src/main.js
+++ src/main.js
@@ -3,10 +3,28 @@ import App from "./App.vue";
 import router from "./router";
 import store from "./store";
 
+import axios from "axios";
+import VueAxios from "vue-axios";
+import vueNumeralFilterInstaller from "vue-numeral-filter";
+
+import { gsap, ScrollTrigger, Draggable, MotionPathPlugin } from "gsap/all";
+
+import "bootstrap/dist/css/bootstrap.min.css";
+import "bootstrap";
+
+gsap.registerPlugin(ScrollTrigger, Draggable, MotionPathPlugin); 
+
+Vue.use(vueNumeralFilterInstaller, { locale: "en-gb" });
+
 Vue.config.productionTip = false;
 
 new Vue({
   router,
   store,
+  vueNumeralFilterInstaller,
+  VueAxios,
+  axios,
   render: (h) => h(App),
 }).$mount("#app");
+
+
